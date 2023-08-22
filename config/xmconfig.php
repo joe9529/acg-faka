cd57be21e8ca7d0e5df612062449cd65453cdfd1
START\jona <jona@gmail>
2023-08-22 23:54:14 +0800

    保存代码

---
+++ config/xmconfig.php
@@ -0,0 +1,13 @@
+<?php
+declare (strict_types=1);
+
+return [
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => 'afka_com',
+    'username' => 'afka_com',
+    'password' => 'pNfc5sr2E3C3aa3r',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'acg_',
+];
